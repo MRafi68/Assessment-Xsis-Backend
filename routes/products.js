6e4d1f1e321233c4256cad9d8e76a3dce2550372
MRafi68 <mrafifadhillah68@gmail.com>
2023-01-06 11:02:42 +0700

    Second Commit

--- routes/products.js
+++ routes/products.js
@@ -38,7 +38,7 @@ router.post('/', async (req, res) => {
     
 });
 
-router.put('/:id', async (req, res) => {
+router.patch('/:id', async (req, res) => {
     const id = req.params.id;
     
     let product = await Product.findByPk(id);
